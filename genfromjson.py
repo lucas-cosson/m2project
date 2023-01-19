e851f839d36c9b50d6e1b43be18c9fc117f1f55e
Lucas Cosson <24441517+lucas-cosson@users.noreply.github.com>
2023-01-19 03:21:13 +0100

    changement de version de carla

--- genfromjson.py
+++ genfromjson.py
@@ -1,8 +1,10 @@
 import json
 
+
 class Vehicle:
-    def __init__(self,name):
-        self.name=name
+    def __init__(self, name):
+        self.name = name
+
 
 f = open("config.json", "r")
 jsonContent = f.read()
@@ -11,21 +13,23 @@ parsedJson = json.loads(jsonContent)[0]
 platoonsJson = parsedJson["Platoon"]
 vehiclesJson = parsedJson["Vehicle"]
 
-numberOfVehicles=0
-platoons={}
-for key,value in platoonsJson.items():
+print(vehiclesJson)
+
+numberOfVehicles = 0
+platoons = {}
+for key, value in platoonsJson.items():
     platoons[key] = []
     for vehicle in value["_children"]:
-        vehicleToAdd=Vehicle(vehicle)
+        vehicleToAdd = Vehicle(vehicle)
 
         vehicleToAdd.parent = key
 
-        #check if the vehicle is the leader of the platoon
-        if(vehicle in value['_delegProv']['leader'][0]):
+        # check if the vehicle is the leader of the platoon
+        if vehicle in value['_delegProv']['leader'][0]:
             vehicleToAdd.role = "Leader"
         else:
             vehicleToAdd.role = "Follower"
-        
+
         vehicleToAdd.battery = vehiclesJson[vehicle]["_parameters"]["battery"]
 
         platoons[key].append(vehicleToAdd)
--- platoon.py
+++ platoon.py
@@ -1,14 +1,8 @@
 import random
-import carla
 from typing import List
-import sys
-import os
 
-import pygame
-try:
-    sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + '/carla')
-except IndexError:
-    pass
+import carla.libcarla as carla
+
 from agents.navigation.behavior_agent import BehaviorAgent
 
 # CONFIGURATION
@@ -16,85 +10,78 @@ NB_VEHICLES = 10
 
 
 def main():
-    global client, actor_list
-    try:
-        actor_list = []
-        agent_list = []
-        
-        client = carla.Client('localhost', 2000)
-        client.set_timeout(20.0)
+    actor_list = []
+    agent_list = []
 
-        #client.start_recorder("recording01.log", True)
+    client = carla.Client('localhost', 2000)
+    client.set_timeout(20.0)
+    try:
+        # client.start_recorder("recording01.log", True)
 
         world: carla.World = client.get_world()
 
-        settings = world.get_settings()
-        settings.synchronous_mode = True # Enables synchronous mode
-        settings.fixed_delta_seconds = 0.05 # Sets the fixed time step
-        world.apply_settings(settings)
-
         print(world.get_map().name)
 
-        if(world.get_map().name != "Carla/map/Town05_Opt"):
+        if world.get_map().name != "Carla/map/Town05_Opt":
             world: carla.World = client.load_world('Town05_Opt')
-        
+
         world.unload_map_layer(carla.MapLayer.All)
 
-        spawn_points: List[carla.Transform] = world.get_map().get_spawn_points()
+        spawn_points = world.get_map().get_spawn_points()
         spawn_point = spawn_points[0]
+        destination = random.choice(spawn_points).location
 
         print('Found %d spawn points.' % len(spawn_points))
 
-        for i in range(NB_VEHICLES):
-            blueprint_library: carla.BlueprintLibrary = world.get_blueprint_library()
-            vehicle_bp: carla.ActorBlueprint = blueprint_library.filter('vehicle')[0]
-            new_spawn_point = spawn_point
-            new_spawn_point.location.y = i * 10
+        vehicle_bp = world.get_blueprint_library().find('vehicle.audi.a2')
+        new_spawn_point = spawn_point
 
-            vehicle: carla.Actor = world.spawn_actor(vehicle_bp, new_spawn_point)
+        vehicle_leader = world.spawn_actor(vehicle_bp, new_spawn_point)
+        vehicle_leader.set_autopilot(False)
+        print(vehicle_leader.id)
+
+        agent = BehaviorAgent(vehicle_leader, behavior='normal')
+        agent.ignore_traffic_lights(True)
+        agent.ignore_stop_signs(True)
+
+        agent.set_destination(destination)
+
+        agent_list.append(agent)
+        actor_list.append(vehicle_leader)
+
+        for i in range(NB_VEHICLES - 1):
+            vehicle_bp = world.get_blueprint_library().find('vehicle.audi.a2')
+            new_spawn_point = spawn_point
+            new_spawn_point.location.y = (i + 1) * 10
+
+            vehicle = world.spawn_actor(vehicle_bp, new_spawn_point)
+            vehicle.set_autopilot(False)
 
             agent = BehaviorAgent(vehicle, behavior='normal')
-            destination = random.choice(spawn_points).location
-            agent.set_destination(destination)
+            agent.ignore_traffic_lights(True)
+            agent.ignore_stop_signs(True)
+
             agent_list.append(agent)
-
             actor_list.append(vehicle)
+
             print('created %s' % vehicle.type_id)
-        
-        clock = pygame.time.Clock()
+
         while True:
-            clock.tick()
-            world.tick()
-
-            #vehicle.apply_control(carla.VehicleControl(throttle=1.0, steer=-1.0))
-
-
-            #control = agent.run_step()
-            #control.manual_gear_shift = False
-            #vehicle.apply_control(control)
-            for (vehicle,agent) in zip(actor_list,agent_list):
-                print("Vehicle %s at %s" % (vehicle.type_id, vehicle.get_location()))
-                # print(vehicle.get_acceleration())
-                # print(vehicle.get_velocity())
+            for (vehicle, agent) in zip(actor_list, agent_list):
                 if agent.done():
                     print("The target has been reached, stopping the simulation")
                     break
-                vehicle.apply_control(agent.run_step())
+                if vehicle.id == vehicle_leader.id:
+                    vehicle.apply_control(agent.run_step())
+                else:
+                    agent.set_destination(vehicle_leader.get_location())
+                    vehicle.apply_control(agent.run_step())
 
-    
-        '''
-            for vehicle in actor_list:
-                print("Vehicle %s at %s" % (vehicle.type_id, vehicle.get_location()))
-                # print(vehicle.get_acceleration())
-                # print(vehicle.get_velocity())
-            res = input()
-            if res == 'q':
-                break
-        '''
+
     finally:
         client.apply_batch([carla.command.DestroyActor(x) for x in actor_list])
-        #client.stop_recorder()
-        #print(client.show_recorder_file_info("recording01.log"))
+        # client.stop_recorder()
+        # print(client.show_recorder_file_info("recording01.log"))
         for actor in actor_list:
             actor.destroy()
         print('Finish simulation')
